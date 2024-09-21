dd89256762abd031820a8f1bf547ff9189c8b6f6
sinanharms <sinan.harms1@gmail.com>
2024-09-21 17:02:36 +0200

    lifted torch<2 restrictions

--- train.py
+++ train.py
@@ -414,9 +414,9 @@ if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
     args.save_iterations.append(args.iterations)
     if args.configs:
-        import mmcv
+        from mmengine.config import Config
         from utils.params_utils import merge_hparams
-        config = mmcv.Config.fromfile(args.configs)
+        config = Config.fromfile(args.configs)
         args = merge_hparams(args, config)
     print("Optimizing " + args.model_path)
 
